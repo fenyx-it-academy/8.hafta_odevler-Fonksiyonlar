bc66d9e3aeb8b75e23e279bf3f3644768ca5f5fd
emrahnl <51508442+emrahnl@users.noreply.github.com>
2019-08-09 21:12:24 +0200

    Update Tire 10.py

--- Tire 10.py
+++ Tire 10.py
@@ -1,7 +1,7 @@
 def ranking():
     words=input("Enter words with (-) sign between them ")
     order=[]
-    order=letter.split("-")
+    order=words.split("-")
     order.sort()
     return print(*order, sep="-")
 ranking()
