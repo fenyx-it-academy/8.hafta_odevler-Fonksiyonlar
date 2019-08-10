3776f4fc52daade336230dc16b458474fa708c94
Ebuyuktas <51675845+Ebuyuktas@users.noreply.github.com>
2019-08-10 13:14:46 +0300

    Update sayı-harf.py

--- sayı-harf.py
+++ sayı-harf.py
@@ -6,7 +6,7 @@ En fazla iki basamaklı bir sayi girebilirsiniz\n""")
 def oku():
     birler=[" ", "bir", "iki", "uc", "dort", "bes", "alti", "yedi", "sekiz", "dokuz"]
     onlar=["on","yirmi", "otuz", "kirk", "elli", "altmis", "yetmis", "seksen", "doksan"]
-    sayi=input("cift basamakalı sayi giriniz: "))
+    sayi=input("cift basamakalı sayi giriniz: ")
     return  onlar[int(sayi[0])-1]+" " +  birler[int(sayi[1])]
 
 print(oku())
