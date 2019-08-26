664aab15c0a2b3b6897a32f03e9382a062087fb6
Selçuk D <51508463+slckmndl@users.noreply.github.com>
2019-08-27 00:07:05 +0200

    Update 12 - Ters Kontrol.py
    Guncellendi..

--- 12 - Ters Kontrol.py
+++ 12 - Ters Kontrol.py
@@ -1,15 +1,18 @@
-def terskontrol(*kelime):
-    for i in kelime:
-        for c in i:
-            if c==c[::-1]:
-                return print("True")
+def tersi(*args):
+    kelime_ters=""
+    sonuc=[]
+    for i in args:
+        for j in range(len(i)-1,-1,-1):
+            kelime_ters+=i[j]
+        print(kelime_ters)
+        if i==kelime_ters:
+            sonuc.append(True)
+            kelime_ters = ""
 
-            else:
-                return False
+        else:
+            sonuc.append(False)
+            kelime_ters = ""
 
+    return sonuc
 
-while True:
-    a = input("KElimeyi Giriniz : ")
-
-    print(terskontrol(a))
 
