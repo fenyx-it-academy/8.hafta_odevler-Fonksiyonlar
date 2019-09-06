0ead2d52d57b1e35893f2a0f3a09507e24b39992
itengineer <51508407+adnngrkly@users.noreply.github.com>
2019-09-06 17:20:00 +0200

    Duzeltme yapildi
    Duzeltildi

--- 11.odev.py
+++ 11.odev.py
@@ -1,8 +1,9 @@
-def ozgun():
-
-    liste=[1,2,3,3,3,3,4,5,5]
+def ozgun(*deger):
+    liste = []
+    for i in deger:
+        liste.append(i)
     kume=set(liste)
     liste=list(kume)
     return liste
 
-print(ozgun())
+print(ozgun(1,1,2,33,44,33,44,55,66,55,6,33,4,567,887,65,65))
