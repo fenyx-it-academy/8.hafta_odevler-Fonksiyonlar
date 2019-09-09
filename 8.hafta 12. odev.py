e59fb31b0952bab794242365a42d609b1223e878
nsraktasnsr <51508732+nsraktasnsr@users.noreply.github.com>
2019-09-09 13:32:30 +0200

    Update 8.hafta 12. odev.py
    gerekli duzeltmeleri yaomaya calistim....uyariniz icin...tsk

--- 8.hafta 12. odev.py
+++ 8.hafta 12. odev.py
@@ -5,16 +5,19 @@ print('b.')
 12-Verilen inputların tersten de aynı olup olmadığını kontrol eden bir fonksiyon yazınız.
 örnek: madam, tacocat, utrecht sonuç: True, True, False
 '''
-def terside_ayni():
-    veri=input("Lutfen Herhangi bir kelime giriniz: ")
-    # ters duz diye bos liste olusturduk
-    duz=[]
-    ters=[]
-    for i in veri:
-        duz.append(i)       # veriyi duz listemize ekledik
-    for k in veri[::-1]:    # veri[::-1] veriyi ters cevirdik
-        ters.append(k)      # ters cevrilen veriyi ters listemize ekledik
-    if duz==ters:           # if ile duz sekli tersine esit mi diye kontrol ettik
-        return True
-    return False
+def terside_ayni(*args):
+    veri=input("Lutfen kelimelerinizi aralarina ',' koyarak giriniz giriniz: ")
+    duz_liste=[]                            # duz diye bos liste olusturduk
+    for i in veri.split(','):
+        duz_liste.append(i)                 # girilen verileri duz listemize ekledik
+    kontrol_listesi=[]
+    for duz in duz_liste:
+        ters= duz[::-1]                     #  [::-1] kelimemizi ters cevirdik
+        print(ters)
+        if duz==ters:                       # if ile duz sekli tersine esit mi diye kontrol ettik
+            kontrol_listesi.append(True)
+        else:
+            kontrol_listesi.append(False)
+    return kontrol_listesi
+
 print(terside_ayni())
